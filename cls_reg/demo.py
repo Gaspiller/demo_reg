45de60a7a6d276ea2f171c49420c95f723189e91
Gaspiller <2574664155@qq.com.>
2025-06-02 01:28:19 +0100

    保存训练模型并添加测试逻辑

--- cls_reg/demo.py
+++ cls_reg/demo.py
@@ -26,3 +26,48 @@ print(Y_train.shape)
 print(X_test.shape)
 print(Y_test.shape)
 
+class Net(torch.nn.Module):
+    def __init__(self, n_feature, n_output):
+        super(Net, self).__init__()
+        self.hidden = torch.nn.Linear(n_feature, 100)
+        self.predict = torch.nn.Linear(100, n_output)
+    def forward(self, x):
+        out = self.hidden(x)
+        out = torch.relu(out)
+        out = self.predict(out)
+        return out
+net = Net(13, 1)
+#loss
+loss_func = torch.nn.MSELoss()
+#optimiter
+optimizer = torch.optim.Adam(net.parameters(), lr=0.01)
+#training
+for i in range(10000):
+    x_data = torch.tensor(X_train, dtype=torch.float32)
+    y_data = torch.tensor(Y_train, dtype=torch.float32)
+    pred = net.forward(x_data)
+    pred = torch.squeeze(pred)
+    loss = loss_func(pred, y_data) * 0.001
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    print("ite:{}, loss_train:{}".format(i, loss))
+    print(pred[0:10])
+    print(y_data[0:10])
+
+    #test
+    x_data = torch.tensor(X_test, dtype=torch.float32)
+    y_data = torch.tensor(Y_test, dtype=torch.float32)
+    pred = net.forward(x_data)
+    pred = torch.squeeze(pred)
+    loss_test = loss_func(pred, y_data) * 0.001
+    print("ite:{}, loss_test:{}".format(i, loss_test))
+
+torch.save(net, "model/model.pkl")
+# torch.load("")
+# torch.save(net.state_dict(), "params.pkl")
+# net.load_state_dict("")
+
+
+
